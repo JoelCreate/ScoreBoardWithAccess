03d03ca5cb76addaff66e900278f9265fe730aa0
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-10 18:43:20 -0700

    working on syncing submit button

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ let accessCode = document.getElementById("code")
 const enterCode = document.getElementById("enter-code")
 const submitBtn = document.getElementById("submit-btn")
 
+submitBtn.addEventListener("click", function(){
+    // if (enterCode.value === accessCode) {
+    //     window.location.href = '/app.html';
+    // }
+    alert("clicked")
+})
+
 
 function enterApp() {
     
     accessCode.innerHTML =  Math.floor(Math.random() * 900000)
 
-    if (enterCode.value === accessCode) {
-        window.location.href = '/app.html';
-    }
+        
 
 }
 
 enterApp()
+
