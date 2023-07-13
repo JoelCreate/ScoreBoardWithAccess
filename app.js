639a09f99838e7a686c33089c60f05fcb78643b5
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-12 17:07:50 -0700

    wired up submit button in JS

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ let accessCode = document.getElementById("code")
 const enterCode = document.getElementById("enter-code")
 const submitBtn = document.getElementById("submit-btn")
 
-submitBtn.addEventListener("click", function(){
-    // if (enterCode.value === accessCode) {
-    //     window.location.href = '/app.html';
-    // }
-    alert("clicked")
+submitBtn.addEventListener("click", function(e){    
+    e.preventDefault()
+    if (enterCode.value === accessCode.value) {
+       // window.open("../app.html")
+       console.log("it matches")
+    }
 })
 
 
-function enterApp() {
-    
-    accessCode.innerHTML =  Math.floor(Math.random() * 900000)
-
-        
-
+function enterApp() {    
+    accessCode.innerHTML =  Math.floor(Math.random() * 900000)  
 }
 
 enterApp()
