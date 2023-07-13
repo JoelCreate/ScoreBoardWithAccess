8a25f21c7f0ca0074676530c9e6c2c35ceb6e3b1
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-12 17:53:27 -0700

    wired up access code to open app when correct access code is entered

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-let accessCode = document.getElementById("code")
-const enterCode = document.getElementById("enter-code")
+let accessCode = document.getElementById("access-code")
+const pinInput = document.getElementById("pin-input")
 const submitBtn = document.getElementById("submit-btn")
 
-submitBtn.addEventListener("click", function(e){    
-    e.preventDefault()
-    if (enterCode.value === accessCode.value) {
-       // window.open("../app.html")
-       console.log("it matches")
-    }
-})
+// let randomNumber =  Math.floor(Math.random() * 900000)   
 
+accessCode.value =  Math.floor(Math.random() * 900000)
 
-function enterApp() {    
-    accessCode.innerHTML =  Math.floor(Math.random() * 900000)  
+function enterApp() {   
+
+    submitBtn.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        const enteredPIN = pinInput.value
+        const correctPIN = accessCode.value
+
+        if (enteredPIN === correctPIN) {
+            window.open("../app.html")
+        } else {
+            alert ("incorrect access code")
+        }
+
+    })
+
 }
 
 enterApp()
